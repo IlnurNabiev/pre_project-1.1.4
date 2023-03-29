60edbf3236b01abea6bd42445bbfa336013c6835
Ilnur Nabiev <mr.cool.nabiev@mail.ru>
2023-03-29 10:59:21 +0300

    Add pp114

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Mikle",  "Scott", (byte) 40);
+        userService.saveUser("Olga", "Nesterova", (byte)30);
+        userService.saveUser("Dima", "Shtanov", (byte)35);
+        userService.saveUser("Anna", "Andreeva", (byte)20);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
